86699447a2937ac669b3c3dd9e61953964a5ddc5
michal <michal@ss2-sit-cpp-test03test2cloudlocal.local>
2017-09-25 10:53:34 +0100

    created first version

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# Performance Test Data Processor
+
+## Purpouse
+
+When working with performance tests you'll most probably require some input test data.
+This might include user logins or any other business related information that need to drive your tests.
+
+To be able to generate significant load on your application you'd like to have multiple machines with load generators.
+E.g. in JMeter world this translates to single master controlling multiple slaves which execute test scenario.
+ 
+Issues might arise when test data is not correctly shared between separate slaves.
+E.g. it might not be desirable to reuse the same login on different machine. 
+Or you can reuse the same set of test data but you'd like to have different _random_ order on each machine.
+
+This small tool is meant to handle those two types of issues: 
+
+- protection between reusing test data across different test nodes 
+- test data randomisation for each node
+ 
+## Usage
+
+    python process_input_data.py
+
+### Additioanal information
+
+- Saves original files in separate files
+- Determines separate test machines based on hostname pattern
+- When data file is not found only warning is outputted to the user. Scripts continues execution
---
+++ process_input_data.py
@@ -0,0 +1,100 @@
+import json
+import random
+import logging
+import math
+import re
+import platform
+
+
+input_files_list = 'input_files.json'
+number_of_slaves = 4
+remote_machine_pattern = 'cpp-test(\\d{2})'
+
+
+class ProcessInputData(object):
+
+    def __init__(self):
+        logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+
+    def main(self):
+        with open(input_files_list) as input_files:
+            files_to_process = json.load(input_files)
+        for filename in files_to_process['files_to_shuffle']:
+            shuffle_lines_in_file(filename)
+        for filename in files_to_process['files_to_split']:
+            process_file_for_part_extraction(filename)
+
+
+def shuffle_lines_in_file(filename):
+    try:
+        archive_file(filename)
+        lines = open(filename).readlines()
+        random.shuffle(lines)
+        open(filename, 'wb').writelines(lines)
+        logging.info("Lines in {} file has been randomly shuffled".format(filename))
+    except IOError:
+        logging.warning("Could not open {} file".format(filename))
+
+def process_file_for_part_extraction(filename):
+    part_number_to_extract = get_number_of_remote_machine()
+    if part_number_to_extract:
+        try:
+            archive_file(filename)
+            extract_part_from_file(part_number_to_extract, filename)
+        except IOError:
+            logging.warning("Could not open {} file".format(filename))
+    else:
+        logging.warning("File split process for {} not started".format(filename));
+
+
+def extract_part_from_file(part_number, filename):
+    file_part_size = int(math.ceil(file_len(filename) / float(number_of_slaves)))
+
+    input_file = open(filename, 'r')
+
+    counter = 0
+    current_file_part = 1
+    output_file = None
+    for line in input_file:
+        if counter % file_part_size == 0:
+            if output_file:
+                output_file.close()
+                output_file = None
+            if current_file_part == part_number:
+                output_file = open(filename, 'wb')
+            current_file_part += 1
+        if output_file: output_file.write(line)
+        counter += 1
+    logging.info("Extracted part number {0} of {1} file".format(int(part_number), filename))
+
+
+def get_number_of_remote_machine():
+    machine_name = platform.node()
+    pattern = remote_machine_pattern
+    m = re.search(pattern, machine_name)
+    if m:
+        found = m.group(1)
+        logging.info("Identified remote machine number:{}".format(found))
+        return int(found)
+    else:
+        logging.warning("Could not identify remote machine number from given hostname: {} ".format(machine_name))
+    return None
+
+
+def archive_file(filename):
+    with open(filename) as loaded_file:
+        lines = loaded_file.read()
+        index = filename.find(".")
+        original_filename = filename[:index] + '_original' + filename[index:]
+        with open(original_filename, 'wb') as output_file:
+            output_file.writelines(lines)
+        logging.info("Original file {0} was saved in {1}".format(filename , original_filename))
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+if __name__ == '__main__':
+    ProcessInputData().main()
